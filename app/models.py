d3fcd31382fcea8de5986e94c5aa1cbbc7766631
p.maslovskaya <p.maslovskaya@tinkoff.ru>
2023-12-05 09:01:21 +0300

    make migrations

--- app/models.py
+++ app/models.py
@@ -34,3 +34,68 @@ class User(Base):
         String(254), nullable=False, unique=True, index=True
     )
     hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
+
+
+from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class Word(Base):
+    __tablename__ = "words"
+    word_id = Column(Integer, primary_key=True)
+    word = Column(Text, nullable=False)
+    language = Column(String(50), nullable=False)
+    last_updated = Column(DateTime, default=datetime.utcnow)
+
+    # Relationships
+    definitions = relationship("Definition", back_populates="word")
+    synonyms = relationship("Synonym", back_populates="word")
+    translations = relationship("Translation", back_populates="word")
+    examples = relationship("Example", back_populates="word")
+
+
+class Definition(Base):
+    __tablename__ = "definitions"
+    definition_id = Column(Integer, primary_key=True)
+    word_id = Column(Integer, ForeignKey("words.word_id"))
+    language = Column(String(50), nullable=False)
+    definition = Column(Text, nullable=False)
+
+    # Relationship
+    word = relationship("Word", back_populates="definitions")
+
+
+class Synonym(Base):
+    __tablename__ = "synonyms"
+    synonym_id = Column(Integer, primary_key=True)
+    word_id = Column(Integer, ForeignKey("words.word_id"))
+    language = Column(String(50), nullable=False)
+    synonym = Column(Text, nullable=False)
+
+    # Relationship
+    word = relationship("Word", back_populates="synonyms")
+
+
+class Translation(Base):
+    __tablename__ = "translations"
+    translation_id = Column(Integer, primary_key=True)
+    word_id = Column(Integer, ForeignKey("words.word_id"))
+    language = Column(String(50), nullable=False)
+    translation = Column(Text, nullable=False)
+
+    # Relationship
+    word = relationship("Word", back_populates="translations")
+
+
+class Example(Base):
+    __tablename__ = "examples"
+    example_id = Column(Integer, primary_key=True)
+    word_id = Column(Integer, ForeignKey("words.word_id"))
+    language = Column(String(50), nullable=False)
+    example = Column(Text, nullable=False)
+
+    # Relationship
+    word = relationship("Word", back_populates="examples")
